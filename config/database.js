d3256b08636e9fcb8ca5efd387c6efdf4b1ec15a
Harshit2786 <harshit.2002.singh@gmail.com>
2024-07-19 21:56:42 +0530

    Postgres db on render

--- config/database.js
+++ config/database.js
@@ -3,12 +3,12 @@
       connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'arshi'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', '2786'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: env.bool('DATABASE_SSL',true), // Not required
           // ssl: {
           //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
           // },
